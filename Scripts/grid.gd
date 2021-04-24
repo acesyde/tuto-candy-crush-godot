6ca99b12a60e14dc828eee868c258fa4383d8945
Pierre-Emmanuel Mercier <acesyde@gmail.com>
2021-04-24 21:25:26 +0200

    pt8

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -96,14 +96,15 @@ func touch_input() -> void:
 		var grid_position: Vector2 = pixel_to_grid(final_touch.x, final_touch.y)
 		if is_in_grid(grid_position.x, grid_position.y) && controlling:
 			touch_difference(pixel_to_grid(first_touch.x, first_touch.y), grid_position)
+			controlling = false
 			
 func swap_pieces(column:int, row:int, direction: Vector2) -> void:
-	var first_piece = all_pieces[column][row]
-	var other_piece = all_pieces[column + direction.x][row + direction.y]
+	var first_piece: piece = all_pieces[column][row]
+	var other_piece: piece = all_pieces[column + direction.x][row + direction.y]
 	all_pieces[column][row] = other_piece
 	all_pieces[column + direction.x][row + direction.y] = first_piece
-	first_piece.position = grid_to_pixel(column + direction.x, row + direction.y)
-	other_piece.position = grid_to_pixel(column, row)
+	first_piece.move(grid_to_pixel(column + direction.x, row + direction.y))
+	other_piece.move(grid_to_pixel(column, row))
 	
 func touch_difference(grid_one: Vector2, grid_two: Vector2) -> void:
 	var difference: Vector2 = grid_two - grid_one
--- Scripts/piece.gd
+++ Scripts/piece.gd
@@ -1,3 +1,18 @@
 extends Node2D
 
+class_name piece
+
 export (String) var color;
+
+onready var move_tween: Tween = $move_tween
+
+func move(target: Vector2) -> void:
+	move_tween.interpolate_property(self, 
+		"position",
+		position, 
+		target, 
+		.3, 
+		Tween.TRANS_ELASTIC, 
+		Tween.EASE_OUT)
+		
+	move_tween.start()
